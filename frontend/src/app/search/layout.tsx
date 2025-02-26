74612a4aeb7cd824cf83d07768d217a8ba2797c3
wisteriahuman <wisteriahuman91@gmail.com>
2025-02-26 21:56:47 +0900

    余計なの入ってた

--- frontend/src/app/search/layout.tsx
+++ frontend/src/app/search/layout.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import Header from "@/components/layout/Header";
-import Footer from "@/components/layout/Footer";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
