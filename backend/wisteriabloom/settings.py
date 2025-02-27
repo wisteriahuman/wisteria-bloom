b7b39718112d06cf9ac33d9bf0e4ce3b897f552e
wisteriahuman <wisteriahuman91@gmail.com>
2025-02-27 13:11:54 +0900

    DEBUGをTrueに

--- backend/wisteriabloom/settings.py
+++ backend/wisteriabloom/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5itm-6gmb%ax4+%1!fb#cr6p15*a%lx-!m!+6dszu68hwt)r*!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "localhost",
