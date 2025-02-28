32b181ce6b33c646e1eaef805ae147e562933b35
wisteriahuman <wisteriahuman91@gmail.com>
2025-02-28 22:51:56 +0900

    fix page of search

--- frontend/src/app/search/page.tsx
+++ frontend/src/app/search/page.tsx
@@ -38,7 +38,7 @@ export default function SearchPage() {
     }, [query])
     return (
         <section className="px-4 py-6">
-            <h1 className="text-3xl font-bold mb-6 text-white">検索結果</h1>
+            <h1 className="text-3xl font-bold mb-6 text-white justify-self-center border-b-2 border-white">検索結果</h1>
             <div className="max-w-4xl mx-auto">
                 {results.length > 0 ? (
                     results.map((result: SearchResult, index) => {
@@ -46,8 +46,8 @@ export default function SearchPage() {
                         ? new URL(result.path, window.location.origin).href
                         : result.path;
                         return (
-                            <div key={index} className="bg-white/50 rounded-lg p-4">
-                                <div className="mb-6 pb-4 border-b border-white">
+                            <div key={index} className="bg-white/50 rounded-lg p-4 mb-4">
+                                <div className="mb-2 pb-4 border-b border-white">
                                     <a
                                         href={absoluteUrl}
                                         className="text-xl text-blue-600 hover:underline"
