0e9fb23440020f7a2a2f5b113a7f3a0ceadfa055
wisteriahuman <wisteriahuman91@gmail.com>
2025-03-01 06:05:24 +0900

    url戻すのわすれ

--- frontend/src/common/urls.tsx
+++ frontend/src/common/urls.tsx
@@ -1,2 +1,2 @@
-// export const SERVERURL = "https://wisteria-bloom-production.up.railway.app/"
-export const SERVERURL = "http://localhost:8000/";
+export const SERVERURL = "https://wisteria-bloom-production.up.railway.app/"
+// export const SERVERURL = "http://localhost:8000/";
